b9d6e6f958c4c2bbd3c58a5531188b52faf6ad35
Roger Lee <rmlee0003@gmail.com>
2019-12-18 20:38:29 -0500

    created get accounts endpoint and sql queries

--- server.js
+++ server.js
@@ -6,4 +6,21 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get("/", async (req,res,next) => {
+try{
+
+ res.json(await db('accounts').select())
+}
+catch(err){
+ next(err)
+}
+})
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Game Over",
+	})
+})
 module.exports = server;
